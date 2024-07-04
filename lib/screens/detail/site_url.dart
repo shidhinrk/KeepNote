755eb318bd582565c42a030cd2c71de6e3c20d8f
SHIDHIN RK <shidhinkv2kgq@gmail.com>
2024-07-04 22:27:23 +0530

    Update site_url.dart

--- lib/screens/detail/site_url.dart
+++ lib/screens/detail/site_url.dart
@@ -1,7 +1,6 @@
 class site_url{
-  String base_url="https://shidhin.in/home/";
-  String test_url="https://shidhin.in/home/test";
-  String load_msg_url="loadmsgdb1/";
-  String add_msg_url="https://shidhin.in/home/addmsgdb/";
-  String delete_msg_url="https://shidhin.in/home/deletemsgdb/";
+  String base_url="";
+  String load_msg_url="";
+  String add_msg_url="";
+  String delete_msg_url="";
 }
